80053ff7677dd34c72c27e48a6e72db8efe85328
Wouter de Bruijn <wouter@hedium.nl>
2025-03-22 17:30:40 +0100

    🎨 Formatting of code

--- Roombie-Arduino/Motor.cpp
+++ Roombie-Arduino/Motor.cpp
@@ -1,6 +1,7 @@
 #include "Motor.h"
 
-Motor::Motor(uint8_t int1, uint8_t int2, uint8_t eep) {
+Motor::Motor(uint8_t int1, uint8_t int2, uint8_t eep)
+{
   int1Pin = int1;
   int2Pin = int2;
   eepPin = eep;
@@ -12,28 +13,35 @@ Motor::Motor(uint8_t int1, uint8_t int2, uint8_t eep) {
   digitalWrite(eepPin, LOW);
 }
 
-void Motor::drive(int16_t speed) {
+void Motor::drive(int16_t speed)
+{
   this->speed = speed;
   this->update();
 }
 
-void Motor::state(bool enabled) {
+void Motor::state(bool enabled)
+{
   digitalWrite(eepPin, enabled);
 }
 
-void Motor::update() {
+void Motor::update()
+{
   bool forward = speed >= 0;
 
-  if (speed == 0) {
+  if (speed == 0)
+  {
     digitalWrite(int1Pin, LOW);
     digitalWrite(int2Pin, LOW);
     return;
   }
 
-  if (forward) {
+  if (forward)
+  {
     analogWrite(int1Pin, speed);
     digitalWrite(int2Pin, LOW);
-  } else {
+  }
+  else
+  {
     digitalWrite(int1Pin, LOW);
     analogWrite(int2Pin, speed * -1);
   }
--- Roombie-Arduino/Motor.h
+++ Roombie-Arduino/Motor.h
@@ -2,18 +2,19 @@
 #define MOTOR_H
 #include <Arduino.h>
 
-class Motor {
-  public:
-    Motor(uint8_t int1, uint8_t int2, uint8_t eep);
-    void drive(int16_t speed);
-    void state(bool enabled);
+class Motor
+{
+public:
+  Motor(uint8_t int1, uint8_t int2, uint8_t eep);
+  void drive(int16_t speed);
+  void state(bool enabled);
 
-  private:
-    void update();
-    int int1Pin;
-    int int2Pin;
-    int eepPin;
-    int speed;
+private:
+  void update();
+  int int1Pin;
+  int int2Pin;
+  int eepPin;
+  int speed;
 };
 
 #endif // MOTOR_H
